2461745da8733ed0f84cf2b9083be294a896f191
zulldesign <admin@zulldesign.ml>
2015-03-01 01:07:35 +0800

    l

---
+++ mydin/Models/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+namespace mydin.Models
+{
+  public class ProductContext : DbContext
+  {
+    public ProductContext()
+      : base("mydin")
+    {
+    }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<CartItem> ShoppingCartItems { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+  }
+}
