80ab1f6ec43f06e1963751b7410337a79f802649
zulldesign <admin@zulldesign.ml>
2015-03-01 22:53:16 +0800

    l

---
+++ WingtipToys/ShoppingCart.aspx.cs
@@ -0,0 +1,19 @@
+﻿using Navigation;
+using System.Collections.Generic;
+using System.Web.UI;
+using WingtipToys.Logic;
+using WingtipToys.Models;
+
+namespace WingtipToys
+{
+	public partial class ShoppingCart : Page
+	{
+		public List<CartItem> GetShoppingCartItems()
+		{
+			ShoppingCartActions actions = new ShoppingCartActions();
+			StateContext.Bag.total = actions.GetTotal();
+			return actions.GetCartItems();
+		}
+
+	}
+}
---
+++ WingtipToys/Site.Master.cs
@@ -0,0 +1,25 @@
+﻿using Navigation;
+using System;
+using System.Linq;
+using System.Web.UI;
+using WingtipToys.Logic;
+using WingtipToys.Models;
+
+namespace WingtipToys
+{
+	public partial class Site : MasterPage
+	{
+		protected void Page_PreRender(object sender, EventArgs e)
+		{
+			ShoppingCartActions usersShoppingCart = new ShoppingCartActions();
+			StateContext.Bag.count = usersShoppingCart.GetCount();
+		}
+
+		public IQueryable<Category> GetCategories()
+		{
+			var db = new WingtipToys.Models.ProductContext();
+			IQueryable<Category> query = db.Categories;
+			return query;
+		}
+	}
+}
